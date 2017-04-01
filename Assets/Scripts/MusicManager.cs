291fa530ec591d9775e17fc8b1adabaa68035a2a
Pedro Paiva <pedrovsp109@gmail.com>
2017-04-01 09:51:19 -0300

    Changing the 'OnLevelWasLoaded' to OnEnable

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class MusicManager : MonoBehaviour {
@@ -16,7 +17,7 @@ public class MusicManager : MonoBehaviour {
 		audioSource = gameObject.GetComponent<AudioSource> ();
 	}
 
-	void OnLevelWasLoaded(int level){
+	/*void OnLevelWasLoaded(int level){
 
 		AudioClip thisLevelMusic = levelMusicChangeArray [level];
 
@@ -25,5 +26,39 @@ public class MusicManager : MonoBehaviour {
 			audioSource.loop = true;
 			audioSource.Play ();
 		}
+	}*/
+
+	void OnEnable()
+	{
+		//Tell our 'OnLevelFinishedLoading' function to start listening for a scene change as soon as this script is enabled.
+		SceneManager.sceneLoaded += OnLevelFinishedLoading;
+	}
+
+	void OnDisable()
+	{
+		//Tell our 'OnLevelFinishedLoading' function to stop listening for a scene change as soon as this script is disabled. Remember to always have an unsubscription for every delegate you subscribe to!
+		SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+	}
+
+	void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+	{
+		AudioClip thisLevelMusic = levelMusicChangeArray [scene.buildIndex];
+
+		if (thisLevelMusic) {
+			audioSource.clip = thisLevelMusic;
+
+			if (scene.buildIndex != 6) {
+				audioSource.loop = true;
+			} else {
+				audioSource.loop = false;
+			}
+					
+		
+			audioSource.Play ();
+		}
+
+		Debug.Log("Level Loaded");
+		Debug.Log(scene.name);
+		Debug.Log(mode);
 	}
 }
