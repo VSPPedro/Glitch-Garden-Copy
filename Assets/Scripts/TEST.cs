1291f5bfd1944913e5590583e10f74c05e4dcf2e
Pedro Paiva <pedrovsp109@gmail.com>
2017-04-05 11:36:24 -0300

    Our PlayerPrefsManager

---
+++ Assets/Scripts/TEST.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print (PlayerPrefsManager.GetMasterVolume ());
+		PlayerPrefsManager.SetMasterVolume (0.3f);
+		print (PlayerPrefsManager.GetMasterVolume ());
+
+		print (PlayerPrefsManager.isLevelUnlocked (2));
+		PlayerPrefsManager.UnLockLevel (2);
+		print (PlayerPrefsManager.isLevelUnlocked (2));
+
+		print (PlayerPrefsManager.GetDifficulty ());
+		PlayerPrefsManager.SetDifficulty(2f);
+		print (PlayerPrefsManager.GetDifficulty ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
