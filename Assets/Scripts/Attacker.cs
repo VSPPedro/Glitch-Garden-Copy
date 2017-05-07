c01ae3c294cea2e4c23196429887b8b4da7b045e
Pedro Paiva <pedrovsp109@gmail.com>
2017-05-07 12:08:13 -0300

    Animating Defender & Projectiles

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -6,15 +6,19 @@ public class Attacker : MonoBehaviour {
 
 	private GameObject currentTarget;
 	private float currentSpeed;
+	private Animator anim;
 
 	// Use this for initialization
 	void Start () {
-		
+		anim = GetComponent<Animator> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
 		transform.Translate (Vector3.left * currentSpeed * Time.deltaTime);
+
+		if (currentTarget)
+			anim.SetBool ("isAttacking", false);
 	}
 
 	void OnTriggerEnter2D()
@@ -29,7 +33,15 @@ public class Attacker : MonoBehaviour {
 
 	public void StrikeCurrentTarget(float damage)
 	{
-		
+		if (currentTarget) 
+		{
+			Health health = currentTarget.GetComponent<Health> ();
+
+			if (health) 
+			{
+				health.DealDamage (damage);
+			}
+		}
 	}
 
 	public void Attack(GameObject obj)
