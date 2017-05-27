6a70e88899dd517c8252d43f56c971f05ef11084
Pedro Paiva <pedrovsp109@gmail.com>
2017-05-27 15:44:27 -0300

    Creating When Needed

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour {
 
-	public GameObject projectile, projectileParent, gun;
+	private const string PROJECTILE_PARENT_NAME =  "Projectile";
+
+	public GameObject projectile, gun;
+
+	private GameObject projectileParent;
+
+	void Start()
+	{
+		projectileParent = GameObject.Find (PROJECTILE_PARENT_NAME);
+
+		if (!projectileParent) 
+		{
+			projectileParent = new GameObject (PROJECTILE_PARENT_NAME);
+		}
+	}
 
 	private void Fire()
 	{
 		GameObject newProjectile = Instantiate (projectile) as GameObject;
 		newProjectile.transform.parent = projectileParent.transform;
-
 		newProjectile.transform.position = gun.transform.position;
 	}
 }
